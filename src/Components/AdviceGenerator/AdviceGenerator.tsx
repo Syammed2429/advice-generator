534561fe0ee8d6fd75c6087ded1f92c64b6b9e80
Dada Khalandar Bellary <kerry.blig12@gmail.com>
2023-06-22 05:16:35 +0530

    completed the desktop design and api integration

--- src/Components/AdviceGenerator/AdviceGenerator.tsx
+++ src/Components/AdviceGenerator/AdviceGenerator.tsx
@@ -1,12 +1,115 @@
-import { Box } from "@chakra-ui/react";
-import { FC } from "react";
+import { Box, Card, CardBody, Center, Text, Flex, Spinner, Image, CardFooter } from "@chakra-ui/react";
+import { FC, useEffect, useState } from "react";
+import axios from "axios";
+import imageDivider from '../../assets/images/pattern-divider-desktop.svg'
+import diceImage from '../../assets/images/icon-dice.svg'
+
 
 export const AdviceGenerator: FC = () => {
+    const [randomAdvice, setRandomAdvice] = useState<string>('');
+    const [adviceId, setAdviceId] = useState<number | null>(null)
+    const [isAdvice, setIsAdvice] = useState<boolean>(true)
+
+    const getRandomAdvice = async () => {
+        setIsAdvice(true)
+        try {
+            const response = await axios.get("https://api.adviceslip.com/advice");
+            const { advice } = response.data.slip;
+            setAdviceId(response.data.slip.id);
+            setRandomAdvice(advice);
+            setIsAdvice(false)
+
+        } catch (error) {
+            console.error("Failed to fetch random advice:", error);
+        }
+    };
+
+    useEffect(() => {
+        getRandomAdvice()
+    }, [randomAdvice]);
+
+
     return (
         <>
-            <Box>
-                Advice
-            </Box>
+            <Center
+                w='100%'
+            >
+                <Flex
+
+                >
+                    <Card
+                        textAlign='center'
+                        w='30rem'
+                    >
+
+                        <Text
+                            color='hsl(150, 100%, 66%)'
+                            letterSpacing={5}
+                            textTransform='uppercase'
+                            mt={10}
+                        >Advice #{adviceId}</Text>
+                        <CardBody
+                            fontSize='28px'
+
+                        >
+                            <Flex
+                                direction='column'
+                                alignItems='center'
+                                flexWrap={'wrap'}
+                            >
+
+                                {isAdvice ? (
+
+                                    <Spinner />
+                                ) :
+                                    <>
+                                        <Text>
+                                            <q>{randomAdvice}</q>
+
+                                        </Text>
+                                    </>
+
+                                }
+                            </Flex>
+                        </CardBody>
+                        <CardFooter>
+                            {/* <Center> */}
+                            <Flex
+                                direction='column'
+                                alignItems='center'
+                            >
+
+                                <Image
+                                    mb={4}
+                                    w='85%'
+                                    src={imageDivider}
+                                    alt='divider'
+                                />
+                                <Box
+                                    onClick={getRandomAdvice}
+                                    position='relative'
+                                    top={12}
+                                    borderRadius='full'
+                                    w='70px'
+                                    h='70px'
+                                    bg='hsl(150, 100%, 66%)'
+                                    display='flex'
+                                    justifyContent='center'
+                                    alignItems='center'
+                                    _hover={{
+                                        cursor: 'pointer', boxShadow: "0 0 20px 1px hsl(150, 100%, 66%)",
+                                    }}
+                                >
+                                    <Image
+                                        src={diceImage}
+                                    />
+                                </Box>
+                            </Flex>
+
+                        </CardFooter>
+                    </Card>
+                </Flex>
+            </Center >
         </>
     );
 }
