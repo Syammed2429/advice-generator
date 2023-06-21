17618c3aec40fb64d8a79949bd3416180fc00251
Dada Khalandar Bellary <kerry.blig12@gmail.com>
2023-06-21 21:59:48 +0530

    initialed the component

---
+++ src/Components/AdviceGenerator/AdviceGenerator.tsx
@@ -0,0 +1,12 @@
+import { Box } from "@chakra-ui/react";
+import { FC } from "react";
+
+export const AdviceGenerator: FC = () => {
+    return (
+        <>
+            <Box>
+                Advice
+            </Box>
+        </>
+    );
+}
