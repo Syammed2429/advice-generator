ec7497a86aa7a9899f080ed8dfdeb5f06b5d4561
Dada Khalandar Bellary <kerry.blig12@gmail.com>
2023-06-21 22:00:18 +0530

    added the css and imported the adviced component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import { AdviceGenerator } from './Components/AdviceGenerator/AdviceGenerator';
 
 function App() {
   return (
     <>
+      <AdviceGenerator />
     </>
   );
 }
